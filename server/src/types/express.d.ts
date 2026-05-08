dd4617a5b08764792d560d6557ef47bbb53d0baf
pradeepapalleti <pradeepapalleti16@gmail.com>
2026-05-08 22:40:00 +0530

    Simplify TypeScript config for Render compatibility

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,13 +1,5 @@
 import { Request } from 'express';
 
-declare global {
-  namespace Express {
-    interface Request {
-      userId?: number;
-    }
-  }
-}
-
 export interface AuthRequest extends Request {
   userId?: number;
 }
